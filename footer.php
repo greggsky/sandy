b012f99437a7d11db4623aed9012ceb0b81da089
Erica <e@energy7.com>
2012-11-11 06:14:04 -0500

    add footer content (static)
    Signed-off-by: Erica <e@energy7.com>

--- footer.php
+++ footer.php
@@ -12,6 +12,25 @@
 
 	</div><!-- #main -->
 
+	<footer id="interoccupy">
+			<div class="about">
+				<p>Occupy Sandy is a coordinated relief effort to help distribute resources & volunteers to help neighborhoods and people affected by Hurricane Sandy. We are a coalition of people & organizations who are dedicated to implementing aid and establishing hubs for neighborhood resource distribution. Members of this coalition are from Occupy Wall Street, 350.org, recovers.org and interoccupy.net.</p>
+			</div>
+			<div class="events">
+				<h3>Next meeting:</h3>
+				<p>None scheduled. Check back for the next call.</p>
+			</div>
+			<div class="contact">
+				<h3>Contact us:</h3>
+				<dl>
+					<dt>General Inquiries:</dt>
+					<dd><a href="mailto:OccupySandy@interoccupy.net"> OccupySandy@interoccupy.net</a></dd>
+					<dt>Press:</dt>
+					<dd><a href="mailto:SandyPress@interoccupy.net"> SandyPress@interoccupy.net</a></dd>
+				</dl>
+			</div>		
+	</footer>
+
 	<footer id="colophon" role="contentinfo">
             <div id="site-generator">
             	<?php if ( $footer = of_get_option('footer_text', 0) ) {
@@ -22,6 +41,7 @@
                 <?php } ?>
 			</div>
 	</footer><!-- #colophon -->
+	
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
