e8abfad241942d11f6a4f3111493d6de533b2143
Charles Johnson <feedback+github@radgeek.com>
2012-11-18 02:36:49 +0000

    Start off on some cache diagnostics. Shelve for the moment.

--- extensions/fusion/occupysandybackend.php
+++ extensions/fusion/occupysandybackend.php
@@ -6,7 +6,7 @@ class OccupySandyBackend {
 	function __construct () {
 		add_action( 'admin_menu', array(&$this, 'admin_menu') );
 		add_action( 'admin_init', array(&$this, 'admin_init') );
-
+		
 		$apiKey = get_option('os_fusiontables_apikey', null);
 		$tableId = get_option('os_fusiontables_tableid', null);
 
@@ -20,14 +20,32 @@ class OccupySandyBackend {
 	}
 
 	function admin_init () {
+		global $wpdb;
+
 		// Register FusionTables Backend settings
 		register_setting( 'os-fusiontables-settings', 'os_fusiontables_apikey' );
 		register_setting( 'os-fusiontables-settings', 'os_fusiontables_tableid' );
-	}
+		register_setting( 'os-fusiontables-settings', 'os_fusiontables_wipe_querycache' );
+	} /* if */
 
 	function options_page () {
 		$apiKey = get_option('os_fusiontables_apikey', null);
 		$tableId = get_option('os_fusiontables_tableid', null);
+		$wipeCache = get_option('os_fusiontables_wipe_querycache', null);
+		$cacheOption = 'fusiontables_querycache_'.$apiKey;
+
+		// Request logged to wipe query cache?
+		if (strlen($wipeCache) > 0) :
+			echo "WIPE CACHE! WIPE CACHE!";
+			update_option('os_fusiontables_wipe_querycache', false);
+			$cacheSetting = 'fusiontables_querycache_'.$wipeCache;
+			update_option($cacheSetting, array());
+			var_dump($cacheSetting); var_dump($cacheOption); var_dump(strcmp($cacheSetting, $cacheOption));
+		endif;
+
+		$cache = get_option($cacheOption, array());
+
+
 		?>
 		<div class="wrap">
 		<div id="icon-options-general" class="icon32"><br/></div>
@@ -39,10 +57,14 @@ class OccupySandyBackend {
 		<tr style="vertical-align: top"><th>API Key:</th> <td><input type="text" name="os_fusiontables_apikey" value="<?php echo esc_attr($apiKey); ?>" placeholder="API key" size="127" />
 		<p style="font-size: smaller; color: #333; max-width: 50em; font-style: italic;">From <a href="https://developers.google.com/fusiontables/docs/v1/using#auth">Fusion Tables documentation:</a> <q>Requests to the Fusion Tables API for public data must be accompanied by an identifier, which can be an API key or an auth token. To acquire an API key, visit the APIs Console. In the Services pane, activate the Fusion Tables API; if the Terms of Service appear, read and accept them. Next, go to the API Access pane. The API key is near the bottom of that pane, in the section titled <q>Simple API Access.</q></q></p></tr>
 		<tr><th>Default Table:</th> <td><input type="text" name="os_fusiontables_tableid" value="<?php echo esc_attr($tableId); ?>" placeholder="table_name" size="127" /></tr>
+
+		<tr><th>Wipe Cache:</th> <td><input type="checkbox" name="os_fusiontables_wipe_querycache" value="<?php echo esc_attr($apiKey); ?>" /> Wipe cache</td></tr>
 		</tbody>
 		</table>
 		<p class="submit"><input class="button-primary" type="submit" name="Submit" value="<?php print __('Save Changes'); ?>" /></p>
 		</form>
+		<h3>Cache Status</h3>
+		<pre><?php var_dump($cache); ?></pre>
 		<h3>Test Output</h3>
 		<?php
 		if ($this->has_data()) :
