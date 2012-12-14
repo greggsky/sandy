7abe935af3aa14fdef795e5948699362f61f1d76
Charles Johnson <feedback+github@radgeek.com>
2012-12-14 16:47:03 +0000

    Backend / JSONP / Map in old column names to minimize switch-over hassle.

--- extensions/fusion/sahanageofeature.class.php
+++ extensions/fusion/sahanageofeature.class.php
@@ -34,13 +34,31 @@ class SahanaGeoFeature {
 		return $ret;
 	}
 
-	public function properties ( $geo = true ) {
+	public function properties ( $geo = true, $mapped = true ) {
 		$pp = $this->data->properties;
 
 		if ($geo) :
 			// Now let's merge in some geometry.
 			$pp->Latitude = $this->latitude();
 			$pp->Longitude = $this->longitude();
+		endif;
+
+		if ($mapped) :
+			foreach ($this->mapped_columns() as $to => $from) :
+				$vals = array();
+				$from = explode('|', $from);
+				foreach ($from as $prop) :
+					if (property_exists($pp, $prop)) :
+						if (is_null($val)) :
+							$vals[] = $pp->$prop;
+						endif;
+					endif;
+				endforeach;
+
+				if (count($vals) > 0) :
+					$pp->$to = implode("\n", $vals);
+				endif;
+			endforeach;
 		endif;
 
 		return $pp;
@@ -75,6 +93,20 @@ class SahanaGeoFeature {
 		endif;
 		return $ret;
 	} /* SahanaGeoFeature::to_table () */
+
+	function mapped_columns () {
+		return array(
+		"Title" => "name",
+		"Address" => "addr",
+		"DateAndTimes" => "open",
+		"Description" => "comments",
+		"Status" => "urgent|needed|no",
+		"Link" => "web",
+		"Region" => "L4",
+		"State" => "L1",
+		);
+	}
+
 
 }
 
